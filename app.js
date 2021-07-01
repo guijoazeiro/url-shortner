58c561e571d47a35cad8008c1362b022ee498fc3
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2021-07-01 19:13:25 -0300

    Add post url route

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const express = require("express")
-require("dotenv").config()
+const express = require("express");
+require("dotenv").config();
 
-const connectDB = require('./config/db')
+const app = express();
 
-const app = express()
+const connectDB = require("./config/db");
 
-connectDB()
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+connectDB();
 
-const port = process.env.PORT
+app.use("/api", require("./routes/url"));
+
+const port = process.env.PORT;
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`)
+  console.log(`Server is running on port: ${port}`);
 });
